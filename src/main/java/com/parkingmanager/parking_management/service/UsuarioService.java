e164f05efad9c6d161d4340e11f3746ffd7093bb
Michael-Andryeer <andryeerm@gmail.com>
2024-08-15 22:45:49 -0300

    Recurso para criar um novo usuario

--- src/main/java/com/parkingmanager/parking_management/service/UsuarioService.java
+++ src/main/java/com/parkingmanager/parking_management/service/UsuarioService.java
@@ -1,12 +1,19 @@
 package com.parkingmanager.parking_management.service;
 
+import com.parkingmanager.parking_management.entity.Usuario;
 import com.parkingmanager.parking_management.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor // Cria um metódo construtor com a variavel UsuarioRepository, como metódo construtor, fazendo a injeção de dep. pelo proprio construtor
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
